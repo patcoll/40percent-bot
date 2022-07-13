80a59fdddfc9995c1d02334f18469980ef47d514
Francis Usher <francis.w.usher@gmail.com>
2022-07-12 19:35:39 -0700

    style: replace type cast with type guard

--- src/utils/callHandlers.ts
+++ src/utils/callHandlers.ts
@@ -1,13 +1,14 @@
+function isRejected(
+  input: PromiseSettledResult<unknown>
+): input is PromiseRejectedResult {
+  return input.status === 'rejected';
+}
+
 export default async function callHandlers(
   ...handlers: Array<Promise<void>>
 ): Promise<void> {
   const results = await Promise.allSettled(handlers);
-  results
-    .filter((result) => result.status === 'rejected')
-    .forEach((result) => {
-      console.log(
-        'Something went wrong:',
-        (result as PromiseRejectedResult).reason
-      );
-    });
+  results.filter(isRejected).forEach((result) => {
+    console.log('Something went wrong:', result.reason);
+  });
 }
