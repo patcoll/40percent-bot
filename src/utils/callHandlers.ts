a4869f0207bd05690c68f9cf940de25c62545086
Francis Usher <francis.w.usher@gmail.com>
2022-07-12 19:32:43 -0700

    style: use forEach instead of for-of

--- src/utils/callHandlers.ts
+++ src/utils/callHandlers.ts
@@ -2,13 +2,12 @@ export default async function callHandlers(
   ...handlers: Array<Promise<void>>
 ): Promise<void> {
   const results = await Promise.allSettled(handlers);
-
-  for (const result of results.filter(
-    (result) => result.status === 'rejected'
-  )) {
-    console.log(
-      'Something went wrong:',
-      (result as PromiseRejectedResult).reason
-    );
-  }
+  results
+    .filter((result) => result.status === 'rejected')
+    .forEach((result) => {
+      console.log(
+        'Something went wrong:',
+        (result as PromiseRejectedResult).reason
+      );
+    });
 }
