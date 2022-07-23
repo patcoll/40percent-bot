910a2efb4565c88e116dee8a7e881a98b7626a00
Francis Usher <francis.w.usher@gmail.com>
2022-07-23 14:45:50 -0700

    refactor: use builder rather than config object

--- src/lib/showDescriptionInputModal.ts
+++ src/lib/showDescriptionInputModal.ts
@@ -17,16 +17,16 @@ async function showDescriptionInputModal(
       .setTitle('Describe')
       .addComponents([
         new ActionRowBuilder<TextInputBuilder>().addComponents([
-          new TextInputBuilder({
-            style: TextInputStyle.Paragraph,
-            customId: 'descriptionTextInput',
-            label: 'Description',
-            minLength: 10,
-            maxLength: 1000,
-            required: true,
-            placeholder:
-              'Enter a brief announcement description for your project. It must not be more than 15 lines long.',
-          }),
+          new TextInputBuilder()
+            .setStyle(TextInputStyle.Paragraph)
+            .setCustomId('descriptionTextInput')
+            .setLabel('Description')
+            .setMinLength(10)
+            .setMaxLength(1000)
+            .setRequired(true)
+            .setPlaceholder(
+              'Enter a brief announcement description for your project. It must not be more than 15 lines long.'
+            ),
         ]),
       ])
   );
