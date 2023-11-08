0863050386735da66bdf680a6433b6eac9b8dbde
Pat Collins <pat@burned.com>
2023-11-08 09:35:16 -0500

    merge 40percent fork (#3)
    * chore: update node and update dependencies
    
    * refactor: removed sync wrappers, handle partials
    
    * refactor: removed index.ts handler
    
    * refactor: error handling
    
    * ci: added ci for eslint and prettier
    
    * fix: replace nonexistent tsconfig ES lib
    
    * fix: eslint error on void handler promises
    
    * chore: update dependencies
    
    * fix: don't use `any` in type signature
    
    * chore: update packages, prettier on all files
    
    * chore: updated node version, updated to discord.js 13
    
    * refactor: async client.on event handlers
    
    * chore: set minimal compatible node version engine
    
    * fix: update deprecated event name
    
    In discordjs v13 the `message` event has been changed to `messageCreate`
    
    https://discordjs.guide/additional-info/changes-in-v13.html#naming-conventions
    
    * fix(project): sort category channels on creation
    
    After a new channel is created for an IC or GB, sort the category.
    This is to eliminate the chore of mods sorting channels every time.
    
    Closes #60
    
    * feat: add bot client id to env config
    
    * feat(projects): use reaction button for approval
    
    * feat(project): improve approve/reject buttons
    
    * feat(project): add buttons to join/leave role
    
    Remove the old emojis and all the rest of the reaction handler code.
    
    * fix(projects): dont' tell mods to sort channels
    
    * fix(projects): add legacy role reactions back in
    
    * feat(showcase): changed the "Original Message" field to a link button
    
    * chore(infra): upgrade actions, also use correct node version
    
    * chore(infra): upgrade eslint-changed-files
    
    * chore(infra): separate out install and build
    
    * chore(infra): scale to 1 node on deploy
    
    ---------
    
    Co-authored-by: evanronnei <evanronnei@gmail.com>
    Co-authored-by: Francis Usher <francis.w.usher@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,102 @@
 import config from './config.js';
 import {
-  Message,
   Client,
+  Message,
+  Intents,
   MessageReaction,
+  PartialMessageReaction,
   User,
-  PartialUser,
+  Interaction,
 } from 'discord.js';
+import handleShowcaseMessage from './handlers/showcase';
+import handleSoundtestMessage from './handlers/soundtest';
 import {
-  handleMessage as handleMessageAsync,
-  handleReaction as handleReactionAsync,
-} from './handlers';
+  handleIcGbRequestMessage,
+  handleIcGbReviewInteraction,
+  handleProjectAnnouncementInteraction,
+  handleProjectAnnouncementReaction,
+} from './handlers/project';
+import fetchPartial from './utils/fetchPartial';
+import callHandlers from './utils/callHandlers.js';
 
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+function messageShouldBeHandled(msg: Message): boolean {
+  // Ignore messages from bots
+  // Ignore messages from DMs
+  return !msg.author.bot && msg.guild?.id === config.FORTIES_GUILD;
+}
 
-client.on('ready', () => {
+function reactionShouldBeHandled(
+  reaction: MessageReaction | PartialMessageReaction,
+  user: User
+) {
+  // Ignore reactions from bots
+  // Ignore reactions from DMs
+  return !user.bot && reaction.message.guild?.id === config.FORTIES_GUILD;
+}
+
+function interactionShouldBeHandled(interaction: Interaction) {
+  // Ignore reactions from bots
+  // Ignore reactions from DMs
+  return !interaction.user.bot && interaction.guildId === config.FORTIES_GUILD;
+}
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'],
+});
+
+client.once('ready', () => {
   console.log('==== READY ====');
 });
 
-// We use these wrappers because typescript eslint will throw a fit if we return
-//  an unresolved promise instead of void in the client handlers.
-function handleMessageSyncWrapper(msg: Message): void {
-  void handleMessageAsync(msg, client);
-}
+client.on('error', (err) => {
+  console.log('Uncaught error:', err);
+});
 
-// We use these wrappers because typescript eslint will throw a fit if we return
-//  an unresolved promise instead of void in the client handlers.
-function handleReactionSyncWrapper(
-  reaction: MessageReaction,
-  user: User | PartialUser,
-  action: 'add' | 'remove'
-): void {
-  void handleReactionAsync(reaction, user, action, client);
-}
+client.on('interactionCreate', async (interaction) => {
+  if (!interactionShouldBeHandled(interaction)) return;
+  if (interaction.isButton()) {
+    await callHandlers(
+      handleIcGbReviewInteraction(interaction, client),
+      handleProjectAnnouncementInteraction(interaction)
+    );
+  }
+});
 
-client.on('message', handleMessageSyncWrapper);
+client.on('messageCreate', async (msg) => {
+  await fetchPartial(msg);
 
-client.on('messageReactionAdd', (messageReaction, user) =>
-  handleReactionSyncWrapper(messageReaction, user, 'add')
-);
+  if (!messageShouldBeHandled(msg)) return;
 
-client.on('messageReactionRemove', (messageReaction, user) =>
-  handleReactionSyncWrapper(messageReaction, user, 'remove')
-);
+  await callHandlers(
+    handleShowcaseMessage(msg, client),
+    handleSoundtestMessage(msg, client),
+    handleIcGbRequestMessage(msg, client)
+  );
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  await Promise.all([fetchPartial(reaction), fetchPartial(user)]);
+
+  if (!reactionShouldBeHandled(reaction, user as User)) return;
+
+  await callHandlers(
+    handleProjectAnnouncementReaction(reaction, user as User, 'add')
+  );
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  await Promise.all([fetchPartial(reaction), fetchPartial(user)]);
+
+  if (!reactionShouldBeHandled(reaction, user as User)) return;
+
+  await callHandlers(
+    handleProjectAnnouncementReaction(reaction, user as User, 'remove')
+  );
+});
 
 void client.login(config.BOT_TOKEN);
